218d26c17a7ff84c7c1f4fb5d6ff745a1ea56bc0
Züger Kilian <kilian.zueger@stud.fhgr.ch>
2025-10-06 10:19:56 +0200

    push

--- IM-Uebungen/etl-boilerplate/solution/110_extract.php
+++ IM-Uebungen/etl-boilerplate/solution/110_extract.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://api.open-meteo.com/v1/forecast?latitude=46.9481,46.8499,47.3667&longitude=7.4474,9.5329,8.55&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall,cloud_cover&temperature_unit=fahrenheit&timezone=auto&forecast_days=1";
+$url = "https://daten.stadt.sg.ch/api/explore/v2.1/catalog/datasets/freie-parkplatze-in-der-stadt-stgallen-pls/records?limit=20";
 
 // Initialisiert eine cURL-Sitzung
 $ch = curl_init($url);
