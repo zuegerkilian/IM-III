ef8c239018b05fd883d7c57a908802b7cf130800
Züger Kilian <kilian.zueger@stud.fhgr.ch>
2025-10-06 10:35:12 +0200

    uptdate
    updtade

--- IM-Uebungen/etl-boilerplate/extract.php
+++ IM-Uebungen/etl-boilerplate/extract.php
@@ -16,19 +16,21 @@
 
 function fetchWeatherData()
 {
-    $url = "https://api.open-meteo.com/v1/forecast?latitude=46.9481,46.8499,47.3667&longitude=7.4474,9.5329,8.55&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall,cloud_cover&temperature_unit=fahrenheit&timezone=auto&forecast_days=1";
-
+    $url = "https://daten.stadt.sg.ch/api/explore/v2.1/catalog/datasets/freie-parkplatze-in-der-stadt-stgallen-pls/records?limit=20";
     // Initialisiert eine cURL-Sitzung
-
+    $ch = curl_init($url);
 
     // Setzt Optionen
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
 
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
-
+    $response = curl_exec($ch);
 
     // Schließt die cURL-Sitzung
-
+    curl_close($ch);
+    print_r($response);
 
     // Dekodiert die JSON-Antwort und gibt Daten zurück
 
